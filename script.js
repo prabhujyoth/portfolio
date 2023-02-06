bb37520ba884edc9283dc9fd39fb946ad3f5de7f
prabhujyoth <110693664+prabhujyoth@users.noreply.github.com>
2023-02-06 14:50:41 +0530

    Design Portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+function redirect1(){
+    window.location.replace("https://www.figma.com/file/ZWFtpPsqncVMccNlL5bztF/Tony-Designs?node-id=9%3A71&t=opXWAPjtvH5wu0li-1");
+}
+function redirect2(){
+    window.location.replace("https://www.figma.com/file/ZWFtpPsqncVMccNlL5bztF/Tony-Designs?node-id=5%3A30&t=AJj9SBX8LtNdgQZO-1");
+}
+function redirect3(){
+    window.location.replace("https://www.figma.com/file/ZWFtpPsqncVMccNlL5bztF/Tony-Designs?node-id=21%3A94&t=AJj9SBX8LtNdgQZO-1");
+}
+function fullproject(){
+    window.location.replace("https://www.figma.com/file/ZWFtpPsqncVMccNlL5bztF/Tony-Designs?node-id=1%3A163&t=AJj9SBX8LtNdgQZO-1");
+}
+function Send(){
+    window.location.href = "mailto:jyoth.antony@gmail.com";
+}
+//I don't know what I'm doing lmao
